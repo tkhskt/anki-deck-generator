4cb0f90a1aa7960206df9a5013f07f738ef2aeb6
tkhskt <luckymortality@gmail.com>
2025-03-02 06:02:02 +0900

    リファクタリング

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,20 +3,15 @@ package com.tkhskt.ankideckgenerator
 import com.tkhskt.ankideckgenerator.dictionary.Dictionary
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
     try {
-        val input = CsvLoader("input.csv").read()
-        val dictionary: Dictionary = Dictionary.eijiro("dictionary/eijiro.txt")
-        val entries = mutableListOf<Dictionary.Entry>()
-        input.map {
-            println("Search: ${it.first()}")
-            async {
-                val entry = dictionary.find(it[0], it.getOrNull(1)?.let { Dictionary.PartOfSpeech.find(it) })
-                entries.addAll(entry)
-            }
-        }.awaitAll()
+        val entries = generateEntries(
+            inputFilePath = "input.csv",
+            dictionaryPath = "dictionary/eijiro.txt"
+        )
         val generator = DeckGenerator(
             fileName = "deck",
             cardSeparator = "tkhskt"
@@ -25,4 +20,23 @@ fun main() = runBlocking {
     } catch (e: Exception) {
         println(e.toString())
     }
+}
+
+private suspend fun generateEntries(
+    inputFilePath: String,
+    dictionaryPath: String,
+): List<Dictionary.Entry> {
+    val input = CsvLoader(inputFilePath).read()
+    val dictionary: Dictionary = Dictionary.eijiro(dictionaryPath)
+    val entries = mutableListOf<Dictionary.Entry>()
+    coroutineScope {
+        input.map {
+            async {
+                println("Search: ${it.first()}")
+                val entry = dictionary.find(it[0], it.getOrNull(1)?.let { Dictionary.PartOfSpeech.find(it) })
+                entries.addAll(entry)
+            }
+        }.awaitAll()
+    }
+    return entries
 }
